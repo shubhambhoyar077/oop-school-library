78a9d18d9c09352288180025b2087dd1425e1677
shubham <shubham.bhoyar077@gmail.com>
2023-05-15 20:25:02 +0530

    Create trim and capitalize decorator

---
+++ decorator.rb
@@ -0,0 +1,35 @@
+require_relative 'nameable'
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+
+  def initialize(nameable)
+    super(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def initialize(nameable)
+    super(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name[0...10]
+  end
+end
