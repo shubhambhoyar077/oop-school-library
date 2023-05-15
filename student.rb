f3e1171ee221b561f6c9f73a5b63e5e700df3eca
shubham <shubham.bhoyar077@gmail.com>
2023-05-15 15:51:22 +0530

    Create student class as per instruction

---
+++ student.rb
@@ -0,0 +1,17 @@
+# Create class Student with the following:
+
+#     Inherits from Person.
+#     Constructor extends parent's constructor by adding @classroom and a parameter for it.
+#     Method play_hooky that returns "¯\(ツ)/¯".
+require_relative 'person'
+
+class Student < Person
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
