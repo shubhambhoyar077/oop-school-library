6ae54b289ad6d81e1237b34d5bae5914853b3f44
shubham <shubham.bhoyar077@gmail.com>
2023-05-15 21:07:22 +0530

    remove other classes

--- decorator.rb
+++ decorator.rb
@@ -10,25 +10,3 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
-
-class CapitalizeDecorator < Decorator
-  def initialize(nameable)
-    super(nameable)
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def initialize(nameable)
-    super(nameable)
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name[0...10]
-  end
-end
