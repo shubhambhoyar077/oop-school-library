4f0f6d65fcaf48f9c587edc54ae3ce13d173a572
shubham <shubham.bhoyar077@gmail.com>
2023-05-15 20:26:32 +0530

    Add given ex as commit

--- person.rb
+++ person.rb
@@ -28,10 +28,10 @@ class Person < Nameable
   end
 end
 
-
-person = Person.new(22, name: 'maximilianus')
-puts person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
+# Given Ex:-
+# person = Person.new(22, name: 'maximilianus')
+# puts person.correct_name
+# capitalizedPerson = CapitalizeDecorator.new(person)
+# puts capitalizedPerson.correct_name
+# capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+# puts capitalizedTrimmedPerson.correct_name
