4f69831d5ea68559bd78f9aac7aa1de7f0d9bb49
shubham <shubham.bhoyar077@gmail.com>
2023-05-15 19:48:05 +0530

    Fix code in person class

--- person.rb
+++ person.rb
@@ -10,17 +10,13 @@ class Person
   end
 
   def can_use_services?
-    return true if of_age? || @parent_permission
-
-    false
+    of_age? || @parent_permission
   end
 
   private
 
   def of_age?
-    return true if @age >= 18
-
-    false
+    @age >= 18
   end
 end
 
