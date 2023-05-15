4d8cd826f1693d662bd53bc15a5eca8f72da5785
shubham <shubham.bhoyar077@gmail.com>
2023-05-15 15:50:55 +0530

    Removed ex

--- person.rb
+++ person.rb
@@ -23,10 +23,3 @@ class Person
     false
   end
 end
-
-# Ex:-
-p = Person.new(13, name: 'Joan', parent_permission: false)
-puts p.can_use_services? # false
-
-p2 = Person.new(18, parent_permission: false)
-puts p2.can_use_services? # true
