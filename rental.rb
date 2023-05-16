33b1a9a249c83033697074090ba45cdb74b21146
shubham <shubham.bhoyar077@gmail.com>
2023-05-16 15:54:36 +0530

    Create rental class to manage person and book connection

--- rental.rb
+++ rental.rb
@@ -1,6 +1,12 @@
 class Rental
-  attr_accessor :date
-  def initialize(date)
+  attr_accessor :date, :book, :person
+  def initialize(book, person, date)
     @date = date
+
+    @book = book
+    @book.rentals << self
+
+    @person = person
+    @person.rentals << self
   end
 end
