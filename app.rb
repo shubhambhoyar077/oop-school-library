ab9db6a72cf79772920388f24d70af75d16f16dc
shubham <shubham.bhoyar077@gmail.com>
2023-05-17 15:37:27 +0530

    Fix linters

--- app.rb
+++ app.rb
@@ -10,11 +10,13 @@ class App
   end
 
   def list_all_books
-    @books.each {|book| puts "Title:- #{book.title}   Author:- #{book.author} \n"}
+    @books.each { |book| puts "Title:- #{book.title}   Author:- #{book.author} \n" }
   end
 
   def list_all_peoples
-    @peoples.each {|people| puts "[#{people.class.name}] Name:- #{people.name} ID:- #{people.id} Age:- #{people.age} \n"}
+    @peoples.each do |people|
+      puts "[#{people.class.name}] Name:- #{people.name} ID:- #{people.id} Age:- #{people.age} \n"
+    end
   end
 
   def create_teacher(age, specialization, name, parent_permission)
@@ -35,10 +37,12 @@ class App
 
   def create_rental
     puts 'Select a book from the following list by number'
-    @books.each.with_index {|book, idx| puts "#{idx}) Book #{book.title} by #{book.author}"}
+    @books.each.with_index { |book, idx| puts "#{idx}) Book #{book.title} by #{book.author}" }
     book_index = gets.chomp.to_i
     puts 'Select a person from the following list by number(not ID)'
-    @peoples.each.with_index {|person, idx| puts "#{idx}) [#{person.class.name}] Name #{person.name}, ID #{person.id}, Age #{person.age}"}
+    @peoples.each.with_index do |person, idx|
+      puts "#{idx}) [#{person.class.name}] Name #{person.name}, ID #{person.id}, Age #{person.age}"
+    end
     person_index = gets.chomp.to_i
     puts 'Date:-'
     date = gets.chomp
@@ -49,9 +53,8 @@ class App
     @peoples.each do |person|
       if person.id == person_id
         puts 'Rentals:-'
-        person.rentals.each {|rental| puts "Date #{rental.date}, Book #{rental.book.title} by #{rental.book.author}"}
+        person.rentals.each { |rental| puts "Date #{rental.date}, Book #{rental.book.title} by #{rental.book.author}" }
       end
     end
   end
-
 end
